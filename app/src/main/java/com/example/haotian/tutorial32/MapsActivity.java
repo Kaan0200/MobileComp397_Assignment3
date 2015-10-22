9a9bea168f2ca053f9d913f0d23ed7d294af9c01
Kaan0200 <aka43@case.edu>
2015-10-22 00:54:36 -0400

    added GPS, except for actual storage

--- app/src/main/java/com/example/haotian/tutorial32/MapsActivity.java
+++ app/src/main/java/com/example/haotian/tutorial32/MapsActivity.java
@@ -1,8 +1,15 @@
 package com.example.haotian.tutorial32;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,6 +18,7 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -19,12 +27,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements LocationListener {
     /* -Take Photos Simply-
     http://developer.android.com/training/camera/photobasics.html
      */
@@ -35,7 +46,10 @@ public class MapsActivity extends FragmentActivity {
     static final int REQUEST_TAKE_PHOTO = 1;
 
     private String mCurrentPhotoPath;
+    private File mCurrentInfoFile;
 
+    private LocationManager locationManager;
+    private String provider;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Button picButton; //takes user to camera
 
@@ -45,7 +59,7 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
-
+        setUpDataFileIfNeeded();
 
         picButton = (Button) findViewById(R.id.photobutton);
 
@@ -72,6 +86,9 @@ public class MapsActivity extends FragmentActivity {
                 }
             }
         });
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
     private void galleryAddPic() {
@@ -80,12 +97,6 @@ public class MapsActivity extends FragmentActivity {
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
     }
 
     /**
@@ -126,17 +137,58 @@ public class MapsActivity extends FragmentActivity {
         mMap.addMarker(new MarkerOptions().position(new LatLng(20, 20)).title("EECS397/600"));
     }
 
+    private void setUpDataFileIfNeeded() {
+        try {
+            // create the file
+            String filename = "Assignment3PhotoData.csv";
+            File file = new File(Environment.getExternalStoragePublicDirectory("Pictures"), filename);
+            file.createNewFile();
+
+
+            // Create the writing stream
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write("Filename, Timestamp, Latitude, Longitude, \n");
+            bw.close();
+
+            MediaScannerConnection.scanFile(this,
+                    new String[]{file.toString()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                            Log.i("ExternalStorage", "Scanned " + path + ":");
+                            Log.i("ExternalStorage", "-> uri=" + uri);
+                        }
+                    });
+
+        } catch (IOException e) {
+            new Exception("bad write to Assignment3Photodata");
+        }
+
+
+    }
+
     /**
      * This is all the stuff that happens when you get back from the camera app
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+        // scan the new picture
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         File f = new File(mCurrentPhotoPath);
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
+
+        //write to the csv with the various info things
+        StringBuilder sb = new StringBuilder();
+        sb.append(mCurrentPhotoPath + ", ");
+        //timestamp
+        Long tsLong = System.currentTimeMillis() / 1000;
+        sb.append(tsLong.toString() + ", ");
+        //
+
 
         // get the thumbnail
         if (requestCode == IMAGE_REQUEST_NUMBER && resultCode == RESULT_OK) {
@@ -161,5 +213,39 @@ public class MapsActivity extends FragmentActivity {
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = "file:" + image.getAbsolutePath();
         return image;
+    }
+
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
     }
 }
